99cf3c0e352e07da705aaceebac89627f5ae34f5
coderajith <coderajith@gmail.com>
2018-01-01 10:47:51 +0530

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,19 +10,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user   # Handle a successful save.
     else
       render 'new'
     end
   end
-
-<<<<<<< HEAD
-    private
-=======
-    privatePP
->>>>>>> 4bf540b750b7193b58606d346f9b33e3bd4dee72
-
+  
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,22 +1,16 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-
-<<<<<<< HEAD
-  test "invalid signup information" do
-    get signup_path
-    assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
-                                         email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
-    end
-    assert_template 'users/new'
-  end
-end
-=======
   test "valid signup information" do
     get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
->>>>>>> 4bf540b750b7193b58606d346f9b33e3bd4dee72
