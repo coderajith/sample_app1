6fbb30af20da9f99bac50632f069849db1f8de01
coderajith <coderajith@gmail.com>
2018-01-09 11:29:36 +0530

    sml change

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => 'AKIAJLVXWIMYPC5HBWGQ',
-      :aws_secret_access_key => '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'
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  'sample-application-ajith'
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
