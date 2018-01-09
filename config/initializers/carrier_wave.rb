877a392b0467ed9ca2687399d0596640b07de9dc
coderajith <coderajith@gmail.com>
2018-01-09 11:13:55 +0530

    changes in production env

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJLVXWIMYPC5HBWGQ'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => 'AKIAJLVXWIMYPC5HBWGQ',
+      :aws_secret_access_key => '8n/5OpTSV3h6QoHXcnXbjf4X6K+6OSDwGuTsks7k'
     }
-    config.fog_directory     =  ENV['sample-application-ajith']
+    config.fog_directory     =  'sample-application-ajith'
   end
 end
