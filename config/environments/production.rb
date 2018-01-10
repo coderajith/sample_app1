b9d68ef18217eef4b67e762e2421d3c951a76f66
coderajith <coderajith@gmail.com>
2018-01-10 10:16:40 +0530

    fix issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://fierce-wave-13045.herokuapp.com'
+  host = 'https://twitts-ror.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
